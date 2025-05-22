559abc5a97cde934361a0dc26a3667a5aeddef2b
Adenuga Oluwatunmise <oluwatunmiseadenuga@gmail.com>
2025-05-22 17:27:44 +0100

    chore: kyc model updates and todo comments

--- src/models/kyc.ts
+++ src/models/kyc.ts
@@ -2,13 +2,31 @@ import { Model, DataTypes } from "sequelize";
 import sequelize from "../config/database";
 import User from "./user";
 
+const kycIdentifier = ['bvn'] as const;
+export type KYCIdentifier = typeof kycIdentifier[number];
+
+const providers = ['dojah', 'prembly'] as const;
+export type KYCProvider = typeof providers[number];
+
+interface SubmittedData {
+  identifierType: KYCIdentifier;
+  identifierValue: string;
+  frontImage?: string;
+  backImage?: string;
+  image: string;
+}
+
 class KYC extends Model {
   public id!: string;
   public userId!: string;
-  public provider!: string;
-  public submittedData!: object;
+  public provider!: KYCProvider;
+  public submittedData!: SubmittedData;
   public providerResponse!: object | null;
+  public kycIdentifier!: KYCIdentifier;
 }
+
+//todo: setup composite  index on the identifierType and identifierValue
+// todo: setup index on userId
 
 KYC.init(
   {
@@ -18,9 +36,10 @@ KYC.init(
       primaryKey: true,
     },
     userId: { type: DataTypes.UUID, allowNull: false },
-    provider: { type: DataTypes.STRING, allowNull: false },
+    provider: { type: DataTypes.ENUM(...providers), allowNull: false },
     submittedData: { type: DataTypes.JSONB, allowNull: false },
     providerResponse: { type: DataTypes.JSONB, allowNull: true },
+    kycIdentifier: { type: DataTypes.ENUM(...kycIdentifier), allowNull: false },
   },
   { sequelize, modelName: "KYCs", timestamps: true }
 );
